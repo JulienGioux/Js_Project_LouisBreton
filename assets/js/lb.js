e6af7496b2e769a1bef2c986eae41e4426858c5e
jgx <jules.gioux@gmail.com>
2020-05-14 12:20:46 +0200

    A la bouffe... JS en test

--- assets/js/lb.js
+++ assets/js/lb.js
@@ -5,11 +5,16 @@ class Product {
         this.name = name;
     }
     my_value = 21;
-    test(qty) {
-        return this.my_value + qty;
+    addProduct(qty) {
+        return this.my_value += qty;
     };
 }
 
+class showCardProduct {
+    constructor() {
+        
+    }
+}
 
 let produit1 = new Product(`Sac à Main moule et crustacés`);
 let produit2 = new Product(`Montre Azur`);
@@ -17,5 +22,6 @@ let produit3 = new Product(`Colier perles d'atlantique`);
 produit1.name;
 let testQty = produit1.test(2);
  console.log(produit1.test(2), produit1.name);
- console.log(produit1.test(3), produit2.name);
- console.log(produit1.test(4), produit3.name);
+ console.log(produit2.test(3), produit2.name);
+ console.log(produit3.test(4), produit3.name);
+ console.log(produit1.my_value);
